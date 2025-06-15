16b0e75f950934ba995e05c9804cf53553bd9219
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 20:42:30 +0900

    Add: OP_ENV.php | PHPDoc

---
+++ OP_ENV.php
@@ -0,0 +1,10 @@
+<?php
+/**	op-core-trait:/OP_ENV.php
+ *
+ * @genesis    2016-06-09  op-core:/Env.class.php separated from OnePiece5.class.php
+ * @rebirth    2025-06-15  op-core-trait:/OP_ENV.php
+ * @version    1.0
+ * @package    op-core-trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
