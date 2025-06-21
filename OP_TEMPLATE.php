653266870c78a1b3f8c48774ffa80aaf5758faa6
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-22 05:24:02 +0900

    Add: OP_TEMPLATE.php | PHPDoc

---
+++ OP_TEMPLATE.php
@@ -0,0 +1,8 @@
+<?php
+/**	op-core-trait:/OP_TEMPLATE.php
+ *
+ * @created    2024-06-28  op-core:/trait/OP_TEMPLATE.php
+ * @moved      2025-06-22  op-core-trait:/OP_TEMPLATE.php
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
