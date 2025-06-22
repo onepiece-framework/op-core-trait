b302ade9b6716f87555dfeee3034d9d93a3179a9
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-22 13:21:33 +0900

    Add: OP_ONEPIECE.php | PHPDoc

---
+++ OP_ONEPIECE.php
@@ -0,0 +1,10 @@
+<?php
+/**	op-core-trait:/OP_ONEPIECE.php
+ *
+ * @created    2025-06-22
+ * @version    1.0
+ * @package    op-core
+ * @subpackage trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
