3033f8a704606580cbc0fc3c127fa7d3bf4f79de
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-24 21:28:44 +0900

    Add: OP_UNIT.php | trait > OP_UNIT

--- OP_UNIT.php
+++ OP_UNIT.php
@@ -10,3 +10,16 @@
  *
  */
 namespace OP;
+
+/**	OP_UNIT
+ *
+ * @created    2019-02-21
+ * @version    1.0
+ * @package    op-core
+ * @subpackage trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait OP_UNIT
+{
+}
