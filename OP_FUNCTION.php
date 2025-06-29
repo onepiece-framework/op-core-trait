330f02aba87d8250acd046e80101b4bfc6738725
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-29 10:37:52 +0900

    Add: OP_FUNCTION.php | trait > OP_FUNCTION

--- OP_FUNCTION.php
+++ OP_FUNCTION.php
@@ -10,3 +10,18 @@
  *
  */
 namespace OP;
+
+/**	OP_FUNCTION
+ *
+ * Wrapper functions. auto load function/*.php.
+ *
+ * @created    2022-10-05
+ * @version    1.0
+ * @package    op-core
+ * @subpackage trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait OP_FUNCTION
+{
+}
