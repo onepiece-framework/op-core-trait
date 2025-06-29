b4fba185a3b331c26829fc810f19008fee981f3b
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-29 10:36:45 +0900

    Add: OP_FUNCTION.php | PHPDoc

---
+++ OP_FUNCTION.php
@@ -0,0 +1,7 @@
+<?php
+/**	op-core-trait:/OP_FUNCTION.php
+ *
+ * @porting    2025-06-15  op-core-trait:/OP_FUNCTION.php
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
