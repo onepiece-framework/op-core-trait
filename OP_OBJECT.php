6a7f5d24ccd5c610ef0502edd2bd90f325f2d9f5
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-29 16:27:25 +0900

    Add: OP_OBJECT.php | trait > OP_OBJECT

--- OP_OBJECT.php
+++ OP_OBJECT.php
@@ -10,3 +10,17 @@
  *
  */
 namespace OP;
+
+/**	OP_OBJECT
+ *
+ * @created    2022-10-05  op-core-7:/OP.class.php > trait > OP_OBJECT
+ * @porting    2025-06-16  op-core-trait:/OP_OBJECT.php
+ * @version    1.0
+ * @package    op-core
+ * @subpackage trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait OP_OBJECT
+{
+}
