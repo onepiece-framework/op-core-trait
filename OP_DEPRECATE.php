dc52042d8bfad7007da375e39fa2942f5684613d
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-01 14:52:37 +0900

    Add: OP_DEPRECATE.php | trait > OP_DEPRECATE

--- OP_DEPRECATE.php
+++ OP_DEPRECATE.php
@@ -10,3 +10,18 @@
  *
  */
 namespace OP;
+
+/**	OP_DEPRECATE
+ *
+ * This trait was created for compatibility.
+ *
+ * @created    2025-07-01
+ * @version    1.0
+ * @package    op-core
+ * @subpackage trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait OP_DEPRECATE
+{
+}
