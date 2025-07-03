5a13403cfab936e28cb131fe428f6a1843b79eee
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-03 16:12:30 +0900

    Add: OP_UNIT_MAPPER.php | trait > OP_UNIT_MAPPER

--- OP_UNIT_MAPPER.php
+++ OP_UNIT_MAPPER.php
@@ -10,3 +10,16 @@
  *
  */
 namespace OP;
+
+/**	OP_UNIT_MAPPER
+ *
+ * @created    2024-06-08
+ * @version    1.0
+ * @package    op-core
+ * @subpackage trait
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait OP_UNIT_MAPPER
+{
+}
