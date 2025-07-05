c1bb3c02ae1adf7cf34b2eef6cb3b7aa1259da45
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-05 19:01:32 +0900

    New: OP_OBJECT :: MetaPath( ?string $path=null, ?bool $url=null )

--- OP_OBJECT.php
+++ OP_OBJECT.php
@@ -34,4 +34,63 @@ trait OP_OBJECT
 	{
 		return OP::Unit('Router');
 	}
+
+	/**	Meta path
+	 *
+	 * <pre>
+	 * // Set meta path.
+	 * OP()->MetaPath()->Set('hoge', '/var/www/htdocs/hoge/');
+	 *
+	 * // Get meta path to full path.
+	 * $full_path = OP()->MetaPath('hoge:/foo/bar'); -> /var/www/htdocs/hoge/foo/bar/
+	 *
+	 * // Get full path to meta path.
+	 * $meta_path = OP()->MetaPath('/var/www/htdocs/hoge/foo/bar'); -> hoge:/foo/bar/
+	 *
+	 * // Get document root path from meta path.
+	 * $url_path  = OP()->MetaPath('hoge:/foo/bar?key=value', true); -> /hoge/foo/bar/?key=value
+	 * </pre>
+	 *
+	 * @created    2022-10-16
+	 * @param      string     $meta
+	 * @param      bool       $url
+	 * @throws    \Exception
+	 * @return    \OP\MetaPath
+	 */
+	static function MetaPath( ?string $path=null, ?bool $url=null )
+	{
+		//	...
+		static $_meta_path;
+
+		//	...
+		if( $path ){
+			$path = trim($path);
+
+			//	Full path to meta path
+			if( $path[0] === '/' ){
+				//	Full path to URL is not support.
+				if( $url ){
+					throw new \Exception("Full path to URL is not support. ($path)");
+				}
+				return MetaPath::Encode($path);
+			}
+
+			//	Decode meta path.
+			if( $url ){
+				// to URL
+				return MetaPath::URL($path);
+			}else{
+				// to File path.
+				return MetaPath::Decode($path);
+			}
+		}
+
+		//	...
+		if(!$_meta_path ){
+			$_meta_path = new MetaPath();
+		}
+
+		//	...
+		return $_meta_path;
+	}
 }
